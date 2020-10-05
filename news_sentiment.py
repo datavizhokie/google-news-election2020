c202c5baa695295da1a907e1a093bc1655010b23
Mateo <matt.wheeler@gmail.com>
2020-10-05 11:40:39 -0600

    clean bones

--- news_sentiment.py
+++ news_sentiment.py
@@ -4,6 +4,10 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 from datetime import datetime, date
 
+
+news_search_string  = '2020 election'
+pages               = 4
+
 def gen_cal_dates(start_date, end_date):
 
     delta = end_date - start_date
@@ -11,29 +15,13 @@ def gen_cal_dates(start_date, end_date):
     
     return datetime_list
     
-#datetime_list = gen_cal_dates(date(2020, 1, 1), date.today())
-datetime_list = gen_cal_dates(date(2020, 9, 30), date.today())
-
-print(f"There are {len(datetime_list)} days in the generated list")
-
-stringdate_list = []
-for i in range(len(datetime_list)):
-    format_date = datetime.strftime(datetime_list[i], "%m/%d/%Y")
-    stringdate_list.append(format_date)
-    
-min_date = stringdate_list[0] 
-max_date = stringdate_list[-1]
-min_date = min_date.replace("/", "-")
-max_date = max_date.replace("/", "-")
-
 
 def googlenews_extract(date_range, num_pages, search_text):
 
-    ''' Use googlenews package to extract top 30 stories per day based on search string '''
+    ''' Use googlenews package to extract top X stories per day based on search string '''
     
     df_days = []
     
-    # loop through date range to ensure equal sample size from each day
     #TODO: if we want to pull multiple years of data, perhaps add multi-threading...not necessary for < ~20 calls
 
     for date in date_range:
@@ -57,19 +45,17 @@ def googlenews_extract(date_range, num_pages, search_text):
         df_days.append(df) 
         appended_data = pd.concat(df_days)
 
+    # Drop duplicate titles
+    appended_data = appended_data.drop_duplicates(subset=['title'])
+
+    # Append to master news df
     df_news = appended_data.reset_index(drop=True).drop(['date'], axis=1)
       
     return df_news
 
-df_news = googlenews_extract(stringdate_list, 2, '2020 election')
-
-## Subset to having description (valid news stories)
-df_news_subset = df_news[df_news.desc != ""]
-
 
 def tokenize_headlines(df):
     
-
     headlines = df.title.tolist()
 
     all_bigrams = []
@@ -88,23 +74,49 @@ def tokenize_headlines(df):
 
     return all_bigrams
 
-#TODO: bigram sentiment
-#all_bigrams = tokenize_headlines(df_news_subset)
-
 
 def sentiment_scores(df, field):
 
     analyzer = SentimentIntensityAnalyzer()
-
     scores = df[field].apply(analyzer.polarity_scores).tolist()
     df_scores = pd.DataFrame(scores)
-
     df_scored = df.join(df_scores, rsuffix='_right')
 
     return df_scored
 
-df_news_subset_scored = sentiment_scores(df_news_subset, 'title')
-df_news_subset_scored2 = sentiment_scores(df_news_subset_scored, 'desc')
-df_news_subset_scored2.rename(columns={'compound': 'compound_title', 'compound_right': 'compound_desc'}, inplace=True)
 
-df_news_subset_scored2.to_csv(f"'Election 2020' News for {min_date} through {max_date} with Sentiment Scores.csv", index=False)
+def main():
+
+    datetime_list = gen_cal_dates(date(2020, 6, 1), date.today())
+
+    stringdate_list = []
+    for i in range(len(datetime_list)):
+        format_date = datetime.strftime(datetime_list[i], "%m/%d/%Y")
+        stringdate_list.append(format_date)
+        
+    min_date = stringdate_list[0] 
+    max_date = stringdate_list[-1]
+    min_date = min_date.replace("/", "-")
+    max_date = max_date.replace("/", "-")
+
+    print(f"There are {len(datetime_list)} days in the generated list from {min_date} to {max_date}")
+
+    # Call API and specify number of pages to extract
+    df_news = googlenews_extract(stringdate_list, pages, news_search_string)
+
+    # Subset to having a description (valid news stories)
+    df_news_subset = df_news[df_news.desc != ""].reset_index(drop=True)
+
+    print(f"There are {len(df_news_subset)} valid stories for search string '{news_search_string}' in the generated dataset (across {pages} pages each day)")
+
+    #TODO: bigram sentiment
+    #all_bigrams = tokenize_headlines(df_news_subset)
+
+    df_news_subset_scored = sentiment_scores(df_news_subset, 'title')
+    df_news_subset_scored2 = sentiment_scores(df_news_subset_scored, 'desc')
+    df_news_subset_scored2.rename(columns={'compound': 'compound_title', 'compound_right': 'compound_desc'}, inplace=True)
+    df_news_subset_scored2.to_csv(f"'Election 2020' News for {min_date} through {max_date} with Sentiment Scores.csv", index=False)
+
+
+if __name__== "__main__" :
+    main()
